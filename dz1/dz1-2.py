9820d72ed7b1180f911efd77cba3927442a4eb95
karmusha <karmusha@gmail.com>
2022-10-16 17:59:54 +0200

    corrected dz1-2

--- dz1/dz1-2.py
+++ dz1/dz1-2.py
@@ -1,9 +1,9 @@
 # --- 2 ---
 # # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-for x in range(0, 1):
-    for y in range(0, 1):
-        for z in range(0, 1):
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
             set_1 = not (x or y or z)
             set_2 = not x and not y and not z
 

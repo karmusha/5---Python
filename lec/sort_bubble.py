88ddb967b425186c47929c9f008b6c367c352218
karmusha <karmusha@gmail.com>
2022-11-14 18:38:32 +0100

    added bubble sort

---
+++ lec/sort_bubble.py
@@ -0,0 +1,58 @@
+# --- Bubble sort ---
+
+# --- with FOR ---
+from random import randint
+ 
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+ 
+ 
+for i in range(N-1):
+    for j in range(N-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+ 
+print(a)
+
+# --- with WHILE ---
+from random import randint
+ 
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+ 
+i = 0
+while i < N - 1:
+    j = 0
+    while j < N - 1 - i:
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+        j += 1
+    i += 1
+ 
+print(a)
+
+# --- for Python ---
+from random import randint
+ 
+def bubble(array):
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if array[j] > array[j+1]:
+                buff = array[j]
+                array[j] = array[j+1]
+                array[j+1] = buff
+ 
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+ 
+print(a)
+bubble(a)
+print(a)

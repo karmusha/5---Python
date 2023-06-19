b68edb23e92467bf2f56bfc65b593f52a3951f10
karmusha <karmusha@gmail.com>
2023-06-19 19:57:29 +0200

    1 done

--- new_sem1/dz1_task6.py
+++ new_sem1/dz1_task6.py
@@ -1,2 +1,17 @@
 # Выведите в консоль таблицу умножения от 2*2 до 9*10 как на школьной тетради
 
+print('Таблица умножения'.center(60))
+
+for i in range(1, 11):
+    for k in range(2, 6):
+        print(f'{k} * ' + f'{i}'.rjust(2) + ' = ' + f'{i * k}\t'.rjust(3), end=' ')
+    print('')
+
+print('')
+
+for i in range(1, 11):
+    for k in range(6, 10):
+        print(f'{k} * ' + f'{i}'.rjust(2) + ' = ' + f'{i * k}\t'.rjust(3), end=' ')
+    print('')  
+
+print('Всё!'.center(60))

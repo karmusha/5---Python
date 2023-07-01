c985821cb31f00beb3a980625f2bd99f092e6437
karmusha <karmusha@gmail.com>
2023-07-01 21:04:23 +0200

    finished dz2_no_duplicates_list

--- new_sem3/dz2_no_duplicates_list.py
+++ new_sem3/dz2_no_duplicates_list.py
@@ -1,3 +1,14 @@
 # Дан список повторяющихся элементов.
 # Вернуть список с дублирующимися элементами.
 # В результирующем списке не должно быть дубликатов.
+
+numbers = [11, 2, 3, 3, 2, 3, 4, 5]
+print(f'{numbers = }')
+
+res = []
+count = 0
+for value in numbers:
+    if numbers.count(value) > 1 and value not in res:
+        res.append(value)
+
+print(f'List of duplicates: {res}')

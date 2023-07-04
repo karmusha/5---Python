6df306d65962fb250c0786ebb78bac8d90cc8ebd
karmusha <karmusha@gmail.com>
2023-07-04 22:48:50 +0200

    finished dz2_matrix_transport

--- new_sem4/dz2_matrix_transport.py
+++ new_sem4/dz2_matrix_transport.py
@@ -1,3 +1,22 @@
 # Напишите функцию для транспонирования матрицы
 
+mat = [[1, 2, 3],
+       [44, 55, 66],
+       [777, 888, 999],
+       [1010, 1111, 1212]] 
 
+print("Your matrix: ")
+for i in mat:
+    print(i)
+
+def transpose_matrix(matrix):
+    trans_matrix = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
+    for a in range(len(matrix)):
+        for b in range(len(matrix[0])):
+            trans_matrix[b][a] = matrix[a][b]
+
+    print("A transposed matrix: ")
+    for i in trans_matrix:
+        print(i)
+
+transpose_matrix(mat)

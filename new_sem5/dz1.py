d527962788fc7e60cb0a72c22ec4fb0048fa4e0e
karmusha <karmusha@gmail.com>
2023-07-10 05:01:52 +0200

    finished dz1

--- new_sem5/dz1.py
+++ new_sem5/dz1.py
@@ -1,3 +1,13 @@
 # Напишите функцию, которая принимает на вход строку — абсолютный путь до файла.
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 
+full_path = 'C:/Users/karmu/Desktop/sales.txt'
+                       
+
+def get_file_path_name_ext(full_path) -> tuple:
+    temp_name, ext = full_path.split('.')
+    path, name = temp_name.rsplit('/', 1)
+    return path, name, ext
+
+
+print(get_file_path_name_ext(full_path))

369c02e401982ea6dcb5f9e5b745963720e5e93c
karmusha <karmusha@gmail.com>
2023-07-16 11:35:35 +0200

    updated dz3

--- new_sem5/dz3.py
+++ new_sem5/dz3.py
@@ -1,17 +1,11 @@
 # Создайте функцию генератор чисел Фибоначчи (см. Википедию)
 
-n = 0
-
 def gen_fibonacci(n):
-    if n == 0:
-        yield 0
-        n += 1
-    if n == 1 or n == 2:
-        yield 1
-        n += 1
-    else:
-        yield (n-1) + (n-2)
-        n+= 1
+    f1, f2 = 0, 1
+    for _ in range(n):
+        f1, f2 = f2, f1 + f2
+    yield f1
 
-for i in range(0, 8):
+
+for i in range(8):
     print(next(gen_fibonacci(i)))

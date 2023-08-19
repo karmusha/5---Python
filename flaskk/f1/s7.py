a39b282c5beca2c72ea18f89241428f52762d960
karmusha <karmusha@gmail.com>
2023-08-19 14:48:36 +0200

    finished flask dz1

--- flaskk/f1/s7.py
+++ flaskk/f1/s7.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 @app.route('/news/')
 def news():
     data = [
-            {'fname': 'Ira', 'lname': 'Kirova', 'age': 20, 'avg_grade': 4.5},
-            {'fname': 'Olga', 'lname': 'Elova', 'age': 19, 'avg_grade': 4.0},
-            {'fname': 'Igor', 'lname': 'Maslov', 'age': 19, 'avg_grade': 4.2},
+            {'title': 'Sed', 'short': 'Sed ut perspiciatis', 'date': '01.01.2020', 'text': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?'},
+            {'title': 'Quis', 'short': 'Quis autem vel eum iure reprehenderit', 'date': '02.02.2020', 'text': 'Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'},
+            {'title': 'Et harum', 'short': 'Et harum quidem rerum facilis', 'date': '03.03.2020', 'text': 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'},
             ]
-    return render_template('news.html', students=data)
+    return render_template('news.html', news=data)
 
 if __name__ == '__main__':
     app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flaskk.f1.s7 import app
+from flaskk.f1.s8 import app
 
 if __name__ == '__main__':
     app.run(debug=True)

f0c21ba406fa1b0b4b731ea451d1392bdc1a9e20
karmusha <karmusha@gmail.com>
2023-09-07 18:50:46 +0200

    updated final_dz but not finished yet

--- flaskk/fastAPI/final_dz.py
+++ flaskk/fastAPI/final_dz.py
@@ -13,6 +13,7 @@
 
 """
 
+from typing import List
 from fastapi import FastAPI, Request
 from pydantic import BaseModel, Field
 from flask_wtf import CSRFProtect
@@ -21,6 +22,7 @@ from fastapi.templating import Jinja2Templates
 
 import databases 
 import sqlalchemy
+from sqlalchemy import ForeignKey
 
 from flaskk.fastAPI import models as ml
 from flaskk.fastAPI import schemas as ss
@@ -44,25 +46,50 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-@app.get("/items/")
+items = sqlalchemy.Table("items", metadata, 
+                        sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True), 
+                        sqlalchemy.Column("name",sqlalchemy.String(32)), 
+                        sqlalchemy.Column("description",sqlalchemy.String(32)), 
+                        sqlalchemy.Column("price",sqlalchemy.Float()), )
+
+orders = sqlalchemy.Table("orders", metadata, 
+                        sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True), 
+                        sqlalchemy.Column("user_id", sqlalchemy.Integer, ForeignKey(ss.User.user_id)),
+
+                        )
+
+users = sqlalchemy.Table("users", metadata, 
+                        sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True), 
+                        
+                        )
+
+@app.get("/items/", response_model=List[ss.Item])
 async def get_items(): 
-    return None
+    query = items.select()
+    return await database.fetch_all(query)
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: int): 
-    return None
+@app.get("/items/{item_id}", response_model=ss.Item) 
+async def read_item(item_id:int): 
+    query = items.select().where(items.c.id == item_id) 
+    return await database.fetch_one(query)
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int): 
-    return None
+@app.put("/items/{item_id}", response_model=ss.Item)
+async def update_item(item_id:int, new_item:ss.ItemIn): 
+    query = items.update().where(items.c.id == item_id).values(**new_item.dict())
+    await database.execute(query)
+    return{**new_item.dict(),"id":item_id}
 
-@app.post("/items/{item_id}")
-async def create_item(item_id: int): 
-    return None
+@app.post("/items/", response_model=ss.Item)
+async def create_user(item:ss.ItemIn): 
+    query = items.insert().values(name=item.name, email=item.email) 
+    last_record_id = await database.execute(query) 
+    return{**item.dict(),"id":last_record_id}
 
 @app.delete("/items/{item_id}")
-async def delete_item(item_id: int): 
-    return None
+async def delete_item(item_id:int, new_item:ItemIn): 
+    query = items.update().where(items.c.id == item_id)
+    await database.execute(query)
+    return {'message': 'Item deleted'}
 
 
 
--- flaskk/fastAPI/schemas.py
+++ flaskk/fastAPI/schemas.py
@@ -1,21 +1,29 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Item(BaseModel): 
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: str = Field(title="Name", max_length=50)
-    price: float = Field(title="Price", gt=0, le=1000000)
     description: str = Field(default=None, title="Description", max_length=1000) 
-    tax: float = Field(0, title="Tax", ge=0, le=25)
+    price: float = Field(title="Price", gt=0, le=1000000)
 
 class Order(BaseModel):
-    user_id: int = Field(title="Price", gt=0, le=1000000)
-    item_id: int = Field(title="Price", gt=0, le=1000000)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: int = Field(title="User_Id", g=0)
+    item_id: int = Field(title="Item_Id", gt=0)
     order_date: datetime = Field(title="Price", default = datetime.utcnow)
     status: str = Field(title="Status", max_length=50)
 
-class User(BaseModel): 
+class User(BaseModel):
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: str = Field(title="Name", max_length=50)
     last_name: str = Field(title="Last Name", max_length=50)
     email: str = Field(title="Last Name", max_length=50)
     password: str = Field(title="Last Name", max_length=50)
+
+class ItemIn(BaseModel):
+    name: str = Field(title="Name", max_length=50)
+    description: str = Field(default=None, title="Description", max_length=1000) 
+    price: float = Field(title="Price", gt=0, le=1000000)
